490586bb08973b71d61b2ee1ded5b0871e9c31a4
Makar Pronin <mpronin@umass.edu>
2022-12-01 15:53:11 +0500

    length of name changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AddAssignmentElem from './components/assignmentWindows/AddAssignmentElem'
 
 const regEvent1 = {id: "2b34", type: "class", name: "Learn React", startTime: "15:15", startDate: "2022-12-1",
 endTime: "12:45", endDate: "2022-12-2", progressBar: 0}; //id should match event id, such as classEvent.id
-const regEvent2 = {id: "2b234", type: "deadline", name: "HHHHHHH HHHHHH HHHHHHHHH", startTime: "15:15", startDate: "2022-11-30",
+const regEvent2 = {id: "2b234", type: "deadline", name: "FINISHED !!!!!!!!!", startTime: "15:15", startDate: "2022-11-30",
 endTime: "16:15", endDate: "2022-11-30", progressBar: 60};
 
 function App() {
